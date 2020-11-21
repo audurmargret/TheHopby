8177b24dea5721e39658cd4a731823c6c4a6fe27
audurmargret <42755272+audurmargret@users.noreply.github.com>
2020-11-21 23:58:04 +0000

    Kemur villa ef tómt í addSession
    Koma upp villuskilaboð ef t.d. titill er tómur

--- src/main/java/is/hi/hbv501g/TheHopby/Controllers/SessionController.java
+++ src/main/java/is/hi/hbv501g/TheHopby/Controllers/SessionController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +43,7 @@ public class SessionController {
 
 
     @RequestMapping(value = "/hobby/addSession", method = RequestMethod.POST)
-    public String addHobby(@Valid @ModelAttribute("sessions") Session session, @RequestParam String action, BindingResult result, HttpSession hSession, Model model) {
+    public String addHobby(@Valid @ModelAttribute("sessions") Session session, BindingResult result, @RequestParam String action, HttpSession hSession, Model model) {
         User loggedInUser = (User) hSession.getAttribute("LoggedInUser");
         if(loggedInUser == null ){
             return "redirect:/";
